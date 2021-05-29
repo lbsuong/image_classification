dddf1e7ea4978d71314b9b63e76d8aa0b73b8f89
TheSon <thesonvo1998@gmail.com>
2021-05-30 00:00:40 +0700

    add max_matrix_kernel

--- source_cuda.py
+++ source_cuda.py
@@ -1,5 +1,9 @@
+from re import U
 import time
 import math
+from unittest import signals
+import numba
+from numba.cuda.stubs import blockDim, threadIdx
 import numpy as np
 from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.utils import to_categorical
@@ -82,10 +86,57 @@ def conv_forward_kernel(input, filters, output, outputHeight, outputWidth):
             output[node, outputRow, outputCol] += input[filterRow + outputRow,
                                                         filterCol + outputCol] * filters[node, filterRow, filterCol]
 
+# hàm này chỉ tìm max - cần thêm một hàm tìm vị trí - ma trận X sẽ bị sửa đổi
+# gía trị max là giá trị nằm ở đầu mảng X - X[0]
+# blkIdx = blockDim.x - 1
+# unfinishBlk = blockDim.x : số lượng những block chưa hoàn thành
+
 
 @cuda.jit
-def matrix_max_kernel(X):
-    print('chua biet lam ntn')
+def matrix_max_kernel(X, blkIdx, unfinsishBlk):
+    # blkIdx = cuda.device_array(shape=(1,1), dtype=np.int)
+    newBlkIdx = cuda.shared.array((1, 1), np.dtype(int))
+    # if cuda.threadIdx.x == 0 and (blkIdx[0] < 0 or blkIdx[0] > cuda.blockDim.x):
+    #     blkIdx[0] = 0
+
+    if cuda.threadIdx == 0:
+        newBlkIdx[0] = blkIdx
+        blkIdx = blkIdx - 1
+    # cuda.threadfence_system()
+    cuda.syncthreads()
+
+    nBefore = cuda.blockDim.x * newBlkIdx[0] * 2
+
+    stride = 1
+    while stride < cuda.blockDim.x:
+        curr_idx = nBefore + stride * cuda.threadIdx.x
+        next_idx = nBefore + stride * cuda.threadIdx.x + stride
+        curr_row = curr_idx // X.shape[1]
+        curr_col = curr_idx % X.shape[1]
+        next_row = next_idx // X.shape[1]
+        next_col = next_idx % X.shape[1]
+
+        if X[curr_row, curr_col] < X[next_row, next_col]:
+            X[curr_row, curr_col] = X[next_row, next_col]
+        cuda.syncthreads()
+        stride = stride * 2
+
+    if cuda.threadIdx.x == 0:
+        if newBlkIdx[0] < cuda.blockDim.x - 1:
+            # chở block phía sau thực hiện xong
+            while unfinsishBlk > newBlkIdx[0] + 1:
+                pass
+            curr_idx = nBefore
+            next_idx = nBefore + 2 * cuda.blockDim.x
+            curr_row = curr_idx // X.shape[1]
+            curr_col = curr_idx % X.shape[1]
+            next_row = next_idx // X.shape[1]
+            next_col = next_idx % X.shape[1]
+            if X[curr_row, curr_col] < X[next_row, next_col]:
+                X[curr_row, curr_col] = X[next_row, next_col]
+            cuda.threadfence()
+        unfinsishBlk = unfinsishBlk - 1
+        cuda.threadfence_system()
 
 
 def main():
--- test.py
+++ test.py
@@ -1,19 +1,13 @@
+from contextlib import nullcontext
 import time
 import math
 import numpy as np
-from tensorflow.keras.optimizers import SGD
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.datasets import mnist
-from numba import jit
 import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 def main():
-    A = np.arange(12).reshape(3,4)
-    print(A)
-    print(A.shape[0])
+    A = np.empty((1,0), np.int)
+    print(5//3)
 
 
 if __name__ == "__main__":
