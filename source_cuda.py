b3a62820bfd65e5ae2f4746ba568ecaf53c6f725
TDSang <trandinhsangkqh@gmail.com>
2021-06-15 17:11:48 +0700

    maxpool_kernel

--- source_cuda.py
+++ source_cuda.py
@@ -137,9 +137,66 @@ def matrix_max_kernel(X, blkIdx, unfinsishBlk):
         unfinsishBlk = unfinsishBlk - 1
         cuda.threadfence_system()
 
+
 @cuda.jit
-def maxpool_forward_kernel(input, poolSize):
-    input_num = input.shape[0]
+def matrix_max_kernel(X, blkIdx, unfinsishBlk):
+    # blkIdx = cuda.device_array(shape=(1,1), dtype=np.int)
+    newBlkIdx = cuda.shared.array((1, 1), np.dtype(int))
+    # if cuda.threadIdx.x == 0 and (blkIdx[0] < 0 or blkIdx[0] > cuda.blockDim.x):
+    #     blkIdx[0] = 0
+
+    if cuda.threadIdx == 0:
+        newBlkIdx[0] = blkIdx
+        blkIdx = blkIdx - 1
+    # cuda.threadfence_system()
+    cuda.syncthreads()
+
+    nBefore = cuda.blockDim.x * newBlkIdx[0] * 2
+
+    stride = 1
+    while stride < cuda.blockDim.x:
+        curr_idx = nBefore + stride * cuda.threadIdx.x
+        next_idx = nBefore + stride * cuda.threadIdx.x + stride
+        curr_row = curr_idx // X.shape[1]
+        curr_col = curr_idx % X.shape[1]
+        next_row = next_idx // X.shape[1]
+        next_col = next_idx % X.shape[1]
+
+        if X[curr_row, curr_col] < X[next_row, next_col]:
+            X[curr_row, curr_col] = X[next_row, next_col]
+        cuda.syncthreads()
+        stride = stride * 2
+
+    if cuda.threadIdx.x == 0:
+        if newBlkIdx[0] < cuda.blockDim.x - 1:
+            # chở block phía sau thực hiện xong
+            while unfinsishBlk > newBlkIdx[0] + 1:
+                pass
+            curr_idx = nBefore
+            next_idx = nBefore + 2 * cuda.blockDim.x
+            curr_row = curr_idx // X.shape[1]
+            curr_col = curr_idx % X.shape[1]
+            next_row = next_idx // X.shape[1]
+            next_col = next_idx % X.shape[1]
+            if X[curr_row, curr_col] < X[next_row, next_col]:
+                X[curr_row, curr_col] = X[next_row, next_col]
+            cuda.threadfence()
+        unfinsishBlk = unfinsishBlk - 1
+        cuda.threadfence_system()
+
+
+@cuda.jit
+def find_max_index_kernel(X, max_value, index):
+    col = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
+    row = cuda.blockIdx.y * cuda.blockDim.y + cuda.threadIdx.y
+
+    if X[row, col] == max_value:
+        index = (row, col)
+
+
+@cuda.jit
+def maxpool_forward_kernel(input, output, poolSize):
+    input_node = input.shape[0]
     input_h = input.shape[1]
     input_w = input.shape[2]
     output_h= input_h/poolSize
@@ -148,16 +205,21 @@ def maxpool_forward_kernel(input, poolSize):
     node = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
     outputRow = cuda.blockIdx.y * cuda.blockDim.y + cuda.threadIdx.y
     outputCol = cuda.blockIdx.z * cuda.blockDim.z + cuda.threadIdx.z
+    # c, r = cuda.grid(2)
 
-    #output = np.zeros(shape=(filters.shape[0], outputHeight, outputWidth))
-
-    if node > filter.shape[0] or outputRow > output_h or outputCol > output_w:
+    if node > input_node or outputRow > output_h or outputCol > output_w:
         return
+    output[node, outputRow, outputCol]= input[node,outputRow*poolSize, outputCol*poolSize]
+    temp_max = input[node,outputRow*poolSize, outputCol*poolSize]
 
-    for filterRow in range(filters.shape[1]):
-        for filterCol in range(filters.shape[2]):
-            output[node, outputRow, outputCol] += input[filterRow + outputRow,
-            filterCol + outputCol] * filters[node, filterRow, filterCol]
+    for filterRow in range(poolSize):
+        for filterCol in range(1,poolSize):
+            if(input[node,outputRow*poolSize + filterRow, outputCol*poolSize + filterCol] > max):
+                temp_max = input[node,outputRow*poolSize + filterRow, outputCol*poolSize + filterCol]
+    output[node, outputRow, outputCol] = temp_max
+    return output
+
+
 
   '''
   Thực hiện lan truyền xuôi qua maxpool layer.
